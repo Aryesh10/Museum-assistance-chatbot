6c767e76bf58b1432b0fcae8dc84204364fcd6e4
Aryesh <aryesh1014@gmail.com>
2025-03-21 03:18:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Museum Bot Project',
+    version='0.0.0',
+    author='Aryesh',
+    author_email='aryesh1014@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
